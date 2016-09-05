5a2d30f2af300ee712306deae746ba6338bc51c4
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-06 00:49:22 +0300

    fix: Force push fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function initRouter(obj, route, type) {
 
 /* istanbul ignore if */
 if (typeof Matreshka === 'function') {
+	test();// eslint-disable-line
     Matreshka.Router = Router;
     Matreshka.initRouter = initRouter;
 }
