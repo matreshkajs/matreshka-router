77605826136506d4c190b2e206ae43cd299edbe0
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-06 12:43:30 +0300

    fix: Testing bundle-deploy

--- tools/bundle-deploy.js
+++ tools/bundle-deploy.js
@@ -1,15 +1,35 @@
 const { execSync } = require('child_process');
 const path = require('path');
+const variablePrefix = 'npm_package_config_deployOnGit_';
+const fields = ['repository', 'branch', 'folder', 'commit', 'script', 'user_name', 'user_email'];
+const cwd = process.cwd();
+const config = {};
+
+for(const field of fields) {
+    const configVar = process.env[`${variablePrefix}${field}`];
+
+    if(!configVar) {
+        throw Error(`deployOnGit requires "${field}" in package config`);
+    }
+
+    config[field] = configVar.replace(/\$([a-zA-Z0-9_]+)/g, (match, envVarName) => {
+        const envVar = process.env[envVarName];
+
+        if(!envVar) {
+            throw Error(`Environment variable ${envVarName} presented at string "${configVar}" is missing`)
+        }
+
+        return envVarName
+    });
+}
 
 execSync(`
-    git clone -b gh-pages https://$GH_TOKEN@github.com/matreshkajs/matreshka-router.git bundle &&
-    npm run bundle &&
-    cd bundle &&
-    git config user.email "andrey.a.gubanov@gmail.com" &&
-    git config user.name "Andrey Gubanov (his digital copy)" &&
+    git clone -b ${config.branch} ${config.repository} ${config.folder} &&
+    ${config.script} &&
+    cd ${config.folder} &&
+    git config user.email "${config.user_email}" &&
+    git config user.name "${config.user_name}" &&
     git add . &&
-    git commit --allow-empty -m $npm_package_version &&
-    git push https://$GH_TOKEN@github.com/matreshkajs/matreshka-router.git gh-pages
-`, {
-    cwd: path.resolve(__dirname, '..')
-});
+    git commit --allow-empty -m "${config.commit}" &&
+    git push ${config.repository} ${config.branch}
+`, { cwd });
