e456a4a4681aca60d325c5771aed07b36e842256
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-06 10:21:06 +0300

    fix: Tell me who you are

--- tools/bundle-deploy.js
+++ tools/bundle-deploy.js
@@ -2,11 +2,11 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 execSync(`
-    git config user.email "andrey.a.gubanov@gmail.com" &&
-    git config user.name "Andrey Gubanov (his digital copy)" &&
     git clone -b gh-pages https://$GH_TOKEN@github.com/matreshkajs/matreshka-router.git bundle &&
     npm run bundle &&
     cd bundle &&
+    git config user.email "andrey.a.gubanov@gmail.com" &&
+    git config user.name "Andrey Gubanov (his digital copy)" &&
     git add . &&
     git commit --allow-empty -m $npm_package_version &&
     git push https://$GH_TOKEN@github.com/matreshkajs/matreshka-router.git gh-pages
